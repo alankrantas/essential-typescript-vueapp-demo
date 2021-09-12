5bd57e43e949e2044c36f53b764ec170b8363af7
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2021-09-12 19:16:24 +0800

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="p-1 bg-secondary text-white text-end">
+    {{ displayText }}
+    <router-link to="/order" class="btn btn-primary m-1">
+      Submit Order
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "Header",
+
+  setup() {
+    return { store: useStore() };
+  },
+
+  computed: {
+    displayText(): string {
+      const count = this.store.state.order.productCount;
+      return count === 0
+        ? "(No Selection)"
+        : `${count} product(s), ` +
+            `$${this.store.state.order.total.toFixed(2)}`;
+    },
+  },
+});
+</script>
---
+++ src/views/OrderDetails.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h3 class="text-center bg-primary text-white p-2">Order Summary</h3>
+    <div class="p-3">
+      <table class="table table-sm table-striped">
+        <thead>
+          <tr>
+            <th>Quantity</th>
+            <th>Product</th>
+            <th class="text-end">Price</th>
+            <th class="text-end">Subtotal</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="line in order.lines" v-bind:key="line.product.id">
+            <td>{{ line.quantity }}</td>
+            <td>{{ line.product.name }}</td>
+            <td class="text-end">${{ line.product.price.toFixed(2) }}</td>
+            <td class="text-end">${{ line.total.toFixed(2) }}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th class="text-end" colSpan="3">Total:</th>
+            <th class="text-end">${{ order.total.toFixed(2) }}</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+    <div class="text-center">
+      <router-link to="/products" class="btn btn-secondary m-1">
+        Back
+      </router-link>
+      <button class="btn btn-primary m-1" @click="submit">Submit Order</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Order } from "../data/entities";
+import { mapState, mapActions } from "vuex";
+import store, { StoreState } from "../store";
+
+export default defineComponent({
+  name: "OrderDetails",
+
+  computed: {
+    ...mapState<StoreState>({
+      order: (state: StoreState) => state.order,
+      id: (state: StoreState) => state.storedId,
+    }),
+  },
+
+  methods: {
+    ...mapActions(["storeOrder"]),
+    submit() {
+      this.storeOrder((order: Order) => {
+        this.$router.push("/summary");
+        console.log(store.getters.storeId);
+        return store.getters.storeId + 1;
+      });
+    },
+  },
+});
+</script>
